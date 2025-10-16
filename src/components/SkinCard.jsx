c721bd4ca70291889857b22abc2e3c4a0e95ef72
OrionTheProgramer <167390483+OrionTheProgramer@users.noreply.github.com>
2025-10-16 17:57:28 -0300

    Corregido el skincard

--- src/components/SkinCard.jsx
+++ src/components/SkinCard.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 // Funcion de prueba no testeada, utiliza parametros para mostrar distintas skins, es reutilizable
 function SkinCard({ id, name, image, price, type, category }) {
   return (
-    <Card className="product-card" style={{ width: '18rem', margin: '10px' }}>
+    <Card border='info' className="product-card" style={{ width: '18rem', margin: '10px' }}>
       <Card.Img className="product-card-img" variant="top" src={image} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
@@ -12,9 +12,10 @@ function SkinCard({ id, name, image, price, type, category }) {
           Tipo: {type} <br />
           Categoria: <img src={category} alt="Categoria" style={{ width: '20px', height: '20px' }} />
         </Card.Text>
-        <Button as={Link} to={`/skin/${id}`} variant="primary" >Ver Detalles</Button>
-        <hr />
-        <Button variant="success">Precio: ${price}</Button>
+        <Button as={Link} to={`/skin/${id}`} variant="primary">Ver Detalles</Button>
+        <Card.Footer>
+          <big className="text-success">Precio: ${price}</big>
+        </Card.Footer>
       </Card.Body>
     </Card>
   );
